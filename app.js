08ec826c1a62faa13908d8dac96d8ca13f1b3d5d
jlisita <lisita_julien@hotmail.fr>
2024-04-20 15:42:31 +0200

    add pokemons data

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 console.log("hello, node 🖐️");
 
 const express = require('express');
+let pokemons = require('./mock-pokemon');
 const app = express();
 const port = 3000;
 
 app.get("/", (req,res) => res.send("hello again, express ! 🖐️"));
+
+// app.get("/api/pokemons/1", (req,res) => res.send("hello Bulbizarre !"));
+
+app.get("/api/pokemons/:id", (req,res) => 
+{
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find(pokemon => pokemon.id === id);
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}!`)
+});
+
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur http://localhost:${port}`))
