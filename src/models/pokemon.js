55543f93d08c9ddaeeac46c0c6afb1ac7ba54542
jlisita <lisita_julien@hotmail.fr>
2024-04-27 01:01:33 +0200

    add constraints

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: {
+          msg: 'Le nom est déjà pris'
+        },
         validate: {
           notEmpty: {msg: 'Le nom doit contenir des caractères.'},
           notNull: {msg: 'Le nom est une propriété requise.'}
--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,5 +1,5 @@
 const { Pokemon} = require('../db/sequelize')
-const {ValidationError} = require('sequelize')
+const {ValidationError, UniqueConstraintError} = require('sequelize')
 
 module.exports = (app) => {
     app.put('/api/pokemons/:id', (req,res) => {
@@ -20,6 +20,10 @@ module.exports = (app) => {
         })
         .catch(error => {
             if(error instanceof ValidationError)
+            {
+                return res.status(400).json({message: error.message, data:error})
+            }
+            if(error instanceof UniqueConstraintError)
             {
                 return res.status(400).json({message: error.message, data:error})
             }
