1e66414ebee466f091a656c278612cc343a859ee
jlisita <lisita_julien@hotmail.fr>
2024-04-29 12:12:19 +0200

    advanced request

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -6,21 +6,31 @@ module.exports = (app) => {
         if(req.query.name)
         {
             const name = req.query.name;
-            return Pokemon.findAll({
+            const limitReq = parseInt(req.query.limit) || 5;
+            if (name.length < 2)
+            {
+                const message = 'Le terme de requête doit contenir au moins 2 caractères'
+                return res.status(400).json({message})
+            }
+            return Pokemon.findAndCountAll({
                 where: {
                     name: {         // 'name' est la propriété du modèle pokémon
                         [Op.like]:`%${name}%` // 'name' est le critère de la recherche
                     }
-                }
+                },
+                order:['name'],
+                limit:limitReq
             })
-            .then(pokemons => {
-                const message = `il y a ${pokemons.length} pokémons qui correspeondent au terme de recherche ${name}`
-                res.json({message,data:pokemons})
+            .then(({count,rows}) => {
+                const message = `il y a ${count} pokémons qui correspondent au terme de recherche ${name}`
+                res.json({message,data:rows})
             })
         }
         else
         {
-            Pokemon.findAll()
+            Pokemon.findAll({
+                order:['name']
+            })
             .then(pokemons => {
                 const message = 'la liste des pokemons a bien été récupérée.'
                 res.json({message, data: pokemons})
